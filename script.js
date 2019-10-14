e81a296aa367969829bbc016fcd90bdb82e40408
AlekseyButyrin <abrahambaduga@gmail.com>
2019-10-14 20:58:24 +0700

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var buttonscount=1
+var buttonscount=1, tmp=0;
 function function_name() {
 	buttonscount++;
-	for(i=buttonscount;i!=i;i++)
+	for(i=buttonscount;i!=buttonscount;i++)
 	{
 		var tmp=0;
 		tmp=tmp+'<input type="button" onclick="function_name()">';
@@ -21,3 +21,17 @@ function function_name() {
  000000
   0000
   */
+/*
+console.log("ЭТА ПРОГРАММА БУДЕТ РАБОТАТЬ ТОЛЬКО С НЕ БОЛЕЕ 20-ТИ ЦИФРАМИ");
+console.log("ЭТА ПРОГРАММА БУДЕТ РАБОТАТЬ ТОЛЬКО С ПОЛОЖИТЕЛЬНЫМИ ЦИФРАМИ")
+var a = 01010101010101010101; // Максимум - 20 символов!!!
+var sum=0;
+	for(i=0;i<20;i++)
+		{
+		sum = sum + a % 10;
+		a = (a - a % 10)/10;
+	}
+
+document.write(sum)
+
+*/
