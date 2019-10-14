dbcf223b0a0bdcf3bc5fa0c62be8cda6336649a1
AlekseyButyrin <abrahambaduga@gmail.com>
2019-10-14 09:57:01 +0700

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-let a=238152, b=0;
-b=(a/10000)+(a/10000%10)+(a/1000%100)+(a/100%1000)+(a/10%10000)+(a/1%100000)
-document.write("<font color=red><i><b><s>",b,"</s></b></i></font>")
+let a=238152, b=0, c=100000, d=10, abc=0;
+// for(i=0;i<20;i++)
+// {
+
+// }
+	b=a/c;
+	console.log("Jeto chislo ravno ", b);
+	b-=(b%1);
+	console.log("Jeto chislo ravno ", b);
+	c=c/10;
+	abc+=b
+for(i=0;i<5;i++)
+{
+	b=a/c;
+	console.log("Jeto chislo ravno ", b);
+	b-=(b%1);
+	console.log("Jeto chislo ravno ", b);
+	b=b%d;
+	console.log("Jeto chislo ravno ", b);
+	c=c/10;
+	abc+=b
+}
+console.log("Jeto chislo ravno!!!!!!!!!!!!!", abc);
+console.log("POCHEMU POLUCHAETSYA DROBNOE CHISLO?!?!?!?!?!??!?!?!???!?!?!?!/?/1///?1///!/1?!/1?!?/?!11!?");
