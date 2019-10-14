3a4f05cf71fb16c640e0fe91b3e531d46524624d
AlekseyButyrin <abrahambaduga@gmail.com>
2019-10-14 11:10:10 +0700

    initial commit

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-let sum=prompt("ТЫ КТО ТАКОЙ, ЧТОБЫ ЧИСЛО НЕ ВВОДИТЬ!?!?!?!?!?!?");
-switch(sum)
+var buttonscount=1
+function function_name() {
+	buttonscount++;
+	for(i=buttonscount;i!=i;i++)
 	{
-	case '1': document.write("САЛО ОТДАЙ!<br>"); console.log("САЛО ОТДАЙ!"); break;
-	case '2': document.write("МАЛО! ДАЙТЕ БОЛЬШЕ!<br>"); console.log("МАЛО! ДАЙТЕ БОЛЬШЕ!"); break;
-
-	case '3': document.write("Манана.<br>"); console.log("Манана."); break;
-	case '4': document.write("МНОГО!<br>"); console.log("МНОГО!"); break;
-	default: document.write("ВЫ НОРМАЛЬНЫЕ ТАМ ВООБЩЕ???<br>"); console.log("ВЫ НОРМАЛЬНЫЕ ТАМ ВООБЩЕ???"); break;
+		var tmp=0;
+		tmp=tmp+'<input type="button" onclick="function_name()">';
 	}
+	return(tmp);
+}
 
-sum=prompt("ТЫ КТО ТАКОЙ, ЧТОБЫ НОМЕР ДНЯ НЕДЕЛИ НЕ ВВОДИТЬ!?!?!?!?!?!?");
-switch(sum)
-	{
-	case '1': document.write("Понедельник.<br>"); console.log("САЛО ОТДАЙ!"); break;
-	case '2': document.write("Вторняк!<br>"); console.log("МАЛО! ДАЙТЕ БОЛЬШЕ!"); break;
-
-	case '3': document.write("Среда.<br>"); console.log("Манана."); break;
-	case '4': document.write("Четверик!<br>"); console.log("МНОГО!"); break;
-	case '5': document.write("Пьятница!<br>"); console.log("МНОГО!"); break;
-	case '6': document.write("Субота!<br>"); console.log("МНОГО!"); break;
-	case '7': document.write("Воскрешение!<br>"); console.log("МНОГО!"); break;
-	default: document.write("Ньэ бевайэд такова днья недели! ЪУЪ!<br>"); console.log("ВЫ НОРМАЛЬНЫЕ ТАМ ВООБЩЕ???"); break;
-	}
+/*
+                               8
+    000                       00
+ 0000000000000000000000000000000
+00000000000000000000000000000000
+ 000000   1   0
+ 000000  1    0
+ 0000000000000
+ 000000
+ 000000
+  0000
+  */
